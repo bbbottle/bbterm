05c35facf45fc99f2c84afe78203f8c9fd344277
zjhou <z@zjh.im>
2021-04-05 18:01:06 +0800

    feat: auto fit to wrapper

--- src/index.js
+++ src/index.js
@@ -5,13 +5,51 @@ import { xtermConfig } from "./config";
 
 import "./style/index.module.scss";
 
+const getTermHeight = (terminal) => {
+  return (
+    terminal.rows * terminal._core._renderService.dimensions.actualCellHeight
+  );
+};
+
+const getTermPadding = (wrapperHeight, terminal) => {
+  return (wrapperHeight - getTermHeight(terminal)) / 2;
+};
+
+const fitWrapper = ($wrapper, terminal) => {
+  while (getTermPadding($wrapper.offsetHeight, terminal) < 10) {
+    terminal.resize(terminal.cols, terminal.rows - 1);
+  }
+  const finalPd = getTermPadding($wrapper.offsetHeight, terminal);
+  $wrapper.style.paddingTop = `${finalPd}px`;
+  $wrapper.style.paddingBottom = `${finalPd}px`;
+};
+
+const prepareWrapper = () => {
+  const $terminalWrapper = document.createElement("div");
+
+  $terminalWrapper.style.height = "100%";
+  $terminalWrapper.style.width = "100%";
+  $terminalWrapper.style.paddingLeft = "10px";
+  $terminalWrapper.style.paddingRight = "10px";
+  $terminalWrapper.style.boxSizing = "border-box";
+  $terminalWrapper.style.background = "#000";
+
+  return $terminalWrapper;
+};
+
 const initTerm = ($dom) => {
+  const $terminalWrapper = prepareWrapper();
+
+  $dom.appendChild($terminalWrapper);
+
   const terminal = new Terminal(xtermConfig);
   const fitAddon = new FitAddon();
   terminal.loadAddon(fitAddon);
-  terminal.open($dom);
+  terminal.open($terminalWrapper);
   terminal.focus();
   fitAddon.fit();
+
+  fitWrapper($terminalWrapper, terminal);
 
   return terminal;
 };
