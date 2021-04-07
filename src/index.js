fd615965e13a910d08ef5cff22e441c7c0ae3768
zjhou <z@zjh.im>
2021-04-07 11:06:26 +0800

    refactor: add bbterminal class

--- src/index.js
+++ src/index.js
@@ -1,61 +1,11 @@
-import { Terminal } from "@bbbottle/xterm/dist/es6/xterm";
-import { FitAddon } from "@bbbottle/xterm/dist/es6/addons/FitAddon";
 import { Shell } from "./shell";
 import { xtermConfig } from "./config";
+import { BBTerminal } from "./terminal";
 
 import "./style/index.module.scss";
 
-const getTermHeight = (terminal) => {
-  return (
-    terminal.rows * terminal._core._renderService.dimensions.actualCellHeight
-  );
-};
-
-const getTermPadding = (wrapperHeight, terminal) => {
-  return (wrapperHeight - getTermHeight(terminal)) / 2;
-};
-
-const fitWrapper = ($wrapper, terminal) => {
-  while (getTermPadding($wrapper.offsetHeight, terminal) < 10) {
-    terminal.resize(terminal.cols, terminal.rows - 1);
-  }
-  const finalPd = getTermPadding($wrapper.offsetHeight, terminal);
-  $wrapper.style.paddingTop = `${finalPd}px`;
-  $wrapper.style.paddingBottom = `${finalPd}px`;
-};
-
-const prepareWrapper = () => {
-  const $terminalWrapper = document.createElement("div");
-
-  $terminalWrapper.style.height = "100%";
-  $terminalWrapper.style.width = "100%";
-  $terminalWrapper.style.paddingLeft = "10px";
-  $terminalWrapper.style.paddingRight = "10px";
-  $terminalWrapper.style.boxSizing = "border-box";
-  $terminalWrapper.style.background = "#000";
-
-  return $terminalWrapper;
-};
-
-const initTerm = ($dom) => {
-  const $terminalWrapper = prepareWrapper();
-
-  $dom.appendChild($terminalWrapper);
-
-  const terminal = new Terminal(xtermConfig);
-  const fitAddon = new FitAddon();
-  terminal.loadAddon(fitAddon);
-  terminal.open($terminalWrapper);
-  terminal.focus();
-  fitAddon.fit();
-
-  fitWrapper($terminalWrapper, terminal);
-
-  return terminal;
-};
-
 export const startShell = async ($dom, commands = [], options = {}) => {
-  const tml = initTerm($dom);
+  const tml = new BBTerminal(xtermConfig);
   const shell = new Shell(tml);
   const { onBeforeRepl = () => null } = options;
 
@@ -85,6 +35,7 @@ export const startShell = async ($dom, commands = [], options = {}) => {
 
   onBeforeRepl(shell, tml);
 
-  // start repl
+  tml.open($dom);
+
   await shell.repl();
 };
---
+++ src/terminal/index.js
@@ -0,0 +1,49 @@
+import { Terminal } from "@bbbottle/xterm/dist/es6/xterm";
+import { FitAddon } from "@bbbottle/xterm/dist/es6/addons/FitAddon";
+
+export class BBTerminal extends Terminal {
+  constructor(config) {
+    super(config);
+    this.fitAddon = new FitAddon();
+    this.loadAddon(this.fitAddon);
+  }
+
+  open($dom) {
+    const $wrapper = this.prepareWrapper();
+    $dom.appendChild($wrapper);
+    super.open($wrapper);
+    this.focus();
+    this.fitAddon.fit();
+    this.fitWrapper($wrapper);
+  }
+
+  getTermHeight() {
+    return this.rows * this._core._renderService.dimensions.actualCellHeight;
+  }
+
+  getTermPadding(wrapperHeight) {
+    return (wrapperHeight - this.getTermHeight()) / 2;
+  }
+
+  prepareWrapper() {
+    const $terminalWrapper = document.createElement("div");
+
+    $terminalWrapper.style.height = "100%";
+    $terminalWrapper.style.width = "100%";
+    $terminalWrapper.style.paddingLeft = "10px";
+    $terminalWrapper.style.paddingRight = "10px";
+    $terminalWrapper.style.boxSizing = "border-box";
+    $terminalWrapper.style.background = "#000";
+
+    return $terminalWrapper;
+  }
+
+  fitWrapper = ($wrapper) => {
+    while (this.getTermPadding($wrapper.offsetHeight) < 10) {
+      this.resize(this.cols, this.rows - 1);
+    }
+    const finalPd = this.getTermPadding($wrapper.offsetHeight);
+    $wrapper.style.paddingTop = `${finalPd}px`;
+    $wrapper.style.paddingBottom = `${finalPd}px`;
+  };
+}
